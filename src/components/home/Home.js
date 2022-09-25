61fe0310cc77d2bf0b62a3551495ff66c9fc1289
edouard <edouard.blais@hotmail.com>
2022-09-24 23:11:47 -0400

    added logic to prevent duplicated crags and areas

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { fetchAllClimbs, fetchAllUsers } from '../firebase/Firebase';
+import { trimSentence, capitalizeFirstLetter } from "../operations/Operations";
 
 const Home = () => {
     const [allData, setAllData] = useState([]);
@@ -32,17 +33,24 @@ const Home = () => {
     }, []);
 
     const searchAll = (input) => {
-        console.log(allData);
+        const trimInput = trimSentence(input)
+        const trimAndCapInput = capitalizeFirstLetter(trimInput);
+
         const resultsOfInterestsArray = [];
+        const avoidAreaDuplicatesArray = [];
+        const avoidCragDuplicatesArray = [];
+
         for (let data of allData) {
-            if (data.name && data.name.startsWith(input)) {
-                resultsOfInterestsArray.push(data.name)
-            } else if (data.climb && data.climb.startsWith(input)) {
-                resultsOfInterestsArray.push(data.climb)
-            } else if (data.area && data.area.startsWith(input)) {
-                resultsOfInterestsArray.push(data.area)
-            } else if (data.crag && data.crag.startsWith(input)) {
-                resultsOfInterestsArray.push(data.crag)
+            if (data.name && (data.name.startsWith(input) || data.name.startsWith(trimAndCapInput))) {
+                resultsOfInterestsArray.push(data.name);
+            } else if (data.climb && (data.climb.startsWith(input) || data.climb.startsWith(trimAndCapInput))) {
+                resultsOfInterestsArray.push(data.climb);
+            } else if (data.area && (data.area.startsWith(input) || data.area.startsWith(trimAndCapInput)) && !avoidAreaDuplicatesArray.includes(data.area)) {
+                avoidAreaDuplicatesArray.push(data.area);
+                resultsOfInterestsArray.push(data.area);
+            } else if (data.crag && (data.crag.startsWith(input) || data.crag.startsWith(trimAndCapInput)) && !avoidCragDuplicatesArray.includes(data.crag)) {
+                avoidCragDuplicatesArray.push(data.crag);
+                resultsOfInterestsArray.push(data.crag);
             }
         }
         setSearchResult(resultsOfInterestsArray);
@@ -53,8 +61,8 @@ const Home = () => {
             <h2>Search for routes, crags, areas or users!</h2>
             <input type='text' onChange={(e) => searchAll(e.target.value)}/>
             <div>
-                {searchResult.map((result) => {
-                   return <div>{result}</div>
+                {searchResult.map((result, index) => {
+                   return <div key={index}>{result}</div>
                 })}
             </div>
         </div>
