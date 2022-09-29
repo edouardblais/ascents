deb81bcf77650e2bf877fc24c9c653004941f50a
edouard <edouard.blais@hotmail.com>
2022-09-28 21:55:19 -0400

    added add to logbook and todolist functions to climb component

--- src/components/areas/crags/crag/climb/Climb.js
+++ src/components/areas/crags/crag/climb/Climb.js
@@ -1,15 +1,46 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { useLocation } from "react-router-dom";
+import AddAscentModal from '../../../../addascent/AddAscentModal';
+import { auth, addClimbToTodoList } from '../../../../firebase/Firebase';
+import { useAuthState } from 'react-firebase-hooks/auth';
 
 const Climb = () => {
     const location = useLocation();
     const chosenClimb = location.state.chosenClimb;
 
-    return (
-        <div>
-            <h2>{chosenClimb.climb}</h2>
-        </div>
-    )
+    const [user, loading, error] = useAuthState(auth);
+    const [displayModal, setDisplayModal] = useState(false)
+
+    const showAddAscentModal = () => {
+        setDisplayModal(true)
+    };
+
+    if (loading) {
+        return (
+          <div>
+            <p>Initialising User...</p>
+          </div>
+        );
+    }
+
+    if (error) {
+        return (
+            <div>
+                <p>Error: {error}</p>
+            </div>
+        );
+    }
+
+    if (user) {
+        return (
+            <div>
+                <h2>{chosenClimb.climb}</h2>
+                <button onClick={() => showAddAscentModal()}>+Tick!</button>
+                <button onClick={() => addClimbToTodoList(chosenClimb, user.email)}>+To-do!</button>
+                {displayModal? <AddAscentModal climb={chosenClimb} useremail={user.email}/> : null}
+            </div>
+        )
+    }
 }
 
 export default Climb;
