46e7830f21200be8b10ed4480386b70fa6bdcce3
edouard <edouard.blais@hotmail.com>
2022-10-03 21:09:22 -0400

    started implementing recentbox component

--- src/components/home/RecentBox.js
+++ src/components/home/RecentBox.js
@@ -1,8 +1,151 @@
-import React from "react";
+import React, { useEffect, useState } from 'react';
+import { fetchAllUsers, auth, getUserInfoByEmail, fetchFollowingUsers, fetchExactClimb } from '../firebase/Firebase';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { useNavigate } from 'react-router-dom';
 
 const RecentBox = () => {
+    let navigate = useNavigate();
+
+    const [user, loading, error] = useAuthState(auth);
+
+    const [friendsOnly, setFriendsOnly] = useState(false);
+
+    const [recentAscents, setRecentAscents] = useState([]);
+    const [recentFollowingAscents, setRecentFollowingAscents] = useState([]);
+
+    useEffect(() => {
+        const alluserdataarray = [];
+        const allusersdata = fetchAllUsers();
+        allusersdata.then((resolveddata) => {
+            const usersdata = resolveddata;
+            usersdata.map((eachuser) => {
+                alluserdataarray.push(...eachuser.logbook);
+            });
+            const sortedAscentsByDate = alluserdataarray.sort((ascent1 ,ascent2) => {
+                return new Date(ascent2.date) - new Date(ascent1.date);
+            });
+            setRecentAscents(sortedAscentsByDate);
+        })
+    }, [])
+
+    useEffect(() => {
+        if (user) {
+            getUserInfoByEmail(user.email)
+                .then((resolvedInfo) => {
+                    return resolvedInfo 
+                })
+                .then((resolvedInfo) => {
+                    return fetchFollowingUsers(resolvedInfo[0].following);
+                })
+                .then((resolvedInfo)=> {
+                    const followingAscents = [];
+                    for (let followeduser of resolvedInfo) {
+                        followingAscents.push(...followeduser.logbook)
+                    }
+                    const sortedFollowingAscentsByDate = followingAscents.sort((ascent1 ,ascent2) => {
+                        return new Date(ascent2.date) - new Date(ascent1.date);
+                    });
+                    setRecentFollowingAscents(sortedFollowingAscentsByDate);
+                })
+                .catch((err) => {
+                    console.log(err)
+                })
+        }
+    }, [user])
+
+    const seeFollowedAscents = () => {
+        if (user) {
+            setFriendsOnly(true);
+        } else {
+            alert('Please sign in or register to follow climbers!')
+        }
+    }
+
+    const seeAllAscents = () => {
+        setFriendsOnly(false);
+    }
+
+    const seeClimb = (climb) => {
+        const climbinfoarray = fetchExactClimb(climb.climb);
+        climbinfoarray.then((resolvedClimbInfo) => {
+            const climbinfo = resolvedClimbInfo[0];
+            navigate('/SearchAreas/SearchCrags/SearchClimbs/Climb', {
+                state: {
+                    chosenClimb:climbinfo
+                }
+            }) 
+        }) 
+    }
+
+    const seeProfile = (email) => {
+        const user = getUserInfoByEmail(email);
+        user.then((resolvedUser) => {
+            const userinfo = resolvedUser[0];
+            navigate('/visitUser', {
+                state: {
+                    otherUserInfo: userinfo,
+                }
+            })
+        })
+    }
+
+    if (loading) {
+        return (
+          <div>
+            <p>Initialising User...</p>
+          </div>
+        );
+      }
+    
+    if (error) {
+        return (
+            <div>
+                <p>Error: {error}</p>
+            </div>
+        );
+    }
+
+    if (user) {
+        return (
+            <div>
+                <h1>Recent Ascents</h1>
+                <h3>Friends</h3>
+                <div>
+                    {recentFollowingAscents.map((ascent, index) => {
+                        return  <div key={index}>
+                                    <div onClick={() => seeClimb(ascent)}>{ascent.climb} {ascent.date}</div> 
+                                    <div onClick={() => seeProfile(ascent.email)}>{ascent.name}</div>
+                                </div>
+                    })}
+                </div>
+                <h3>Global</h3>
+                <div>
+                {recentAscents.map((ascent, index) => {
+                    return  <div key={index}>
+                                <div onClick={() => seeClimb(ascent)}>{ascent.climb} {ascent.date}</div> 
+                                <div onClick={() => seeProfile(ascent.email)}>{ascent.name}</div>
+                            </div>
+                })}
+            </div>
+            </div>
+        )
+    }
+    
     return (
-        <h2>Recent Ascents</h2>
+        <div>
+            <h1>Recent Ascents</h1>
+            <h3>Friends</h3>
+            
+            <h3>Global</h3>
+            <div>
+                {recentAscents.map((ascent, index) => {
+                    return  <div key={index}>
+                                <div onClick={() => seeClimb(ascent)}>{ascent.climb} {ascent.date}</div> 
+                                <div onClick={() => seeProfile(ascent.email)}>{ascent.name}</div>
+                            </div>
+                })}
+            </div>
+        </div>
     )
 }
 
