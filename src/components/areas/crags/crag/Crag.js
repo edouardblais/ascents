c2420db1f53af1157db52157f9ae7c97e191bd3c
edouard <edouard.blais@hotmail.com>
2022-10-05 15:14:18 -0400

    finished basic implementation of sorting functionalities for crag component and recentbox functionalities for recentbox component

--- src/components/areas/crags/crag/Crag.js
+++ src/components/areas/crags/crag/Crag.js
@@ -33,7 +33,10 @@ const Crag = () => {
                 for (let climb of resolvedClimbs) {
                         climbsToDisplay.push(climb)
                     }
-                setClimbs(climbsToDisplay);
+                const sortedClimbsByAscents = climbsToDisplay.sort((ascent1 ,ascent2) => {
+                    return ((ascent2.numberoflogs) - (ascent1.numberoflogs));
+                });
+                setClimbs(sortedClimbsByAscents);
             })
     }, [])
 
@@ -52,15 +55,31 @@ const Crag = () => {
         }
     }, [climbsToSearch]);
 
+    const sortByAscents = () => {
+        const sortedClimbsByAscents = climbs.sort((ascent1 ,ascent2) => {
+            return ((ascent2.numberoflogs) - (ascent1.numberoflogs));
+        });
+        setClimbs(sortedClimbsByAscents)
+    }
+
+    const sortByRating = () => {
+        const sortedClimbsByRating = climbs.sort((ascent1 ,ascent2) => {
+            return ((ascent2.averagerating) - (ascent1.averagerating));
+        });
+        setClimbs(sortedClimbsByRating)
+    }
+
+    const sortByGrade = () => {
+        const sortedClimbsByGrade = climbs.sort((ascent1 ,ascent2) => {
+            return ((ascent2.grade) - (ascent1.grade));
+        });
+        setClimbs(sortedClimbsByGrade)
+    }
+
     return (
         <div>
             <h2>{chosenCrag.crag}</h2>
             <h3>Climbs</h3>
-            <ul>
-                {climbs.map((climb, index) => {
-                    return <li key={index} onClick={() => linkToClimb(climb)}>{climb.climb}</li>
-                })}
-            </ul>
             <div>Search Climbs</div>
             <input type='text' onChange={(e) => searchClimb(e.target.value)}></input>
             <div>
@@ -68,6 +87,17 @@ const Crag = () => {
                     return <div key={index} onClick={() => linkToClimb(climb)}>{climb.climb}</div>
                 })}
             </div>
+            <div>
+                <p>Sort by:</p>
+                <p onClick={sortByAscents}>Ascents</p>
+                <p onClick={sortByRating}>Rating</p>
+                <p onClick={sortByGrade}>Grade</p>
+            </div>
+            <ul>
+                {climbs.map((climb, index) => {
+                    return <li key={index} onClick={() => linkToClimb(climb)}>{climb.climb} {climb.numberoflogs}</li>
+                })}
+            </ul>
         </div>
     )
 }
