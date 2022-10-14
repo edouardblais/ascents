08b60c07142c348b5791ad4418fe177726f7acb0
edouard <edouard.blais@hotmail.com>
2022-10-13 22:14:46 -0400

    started implementing a find routes by filtering functionality for home component. Need to implement the logic with firebase component.

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -11,6 +11,11 @@ const Home = () => {
     const [allData, setAllData] = useState([]);
     const [goodClimbs, setGoodClimbs] = useState([]);
 
+    const [type, setType] = useState('');
+    const [gradeFrom, setGradeFrom] = useState('');
+    const [gradeTo, setGradeTo] = useState('');
+    const [minRating, setMinRating] = useState('');
+
     useEffect(() => {
         fetchGoodClimbs()
             .then((resolvedgoodclimbs) => {
@@ -108,19 +113,121 @@ const Home = () => {
         })
     }
 
+    const getSelectedType = (typevalue) => {
+        setType(typevalue);
+    };
+
+    const getSelectedGradeFrom = (gradefrom) => {
+        setGradeFrom(gradefrom)
+    }
+
+    const getSelectedGradeTo = (gradeto) => {
+        setGradeTo(gradeto)
+    }
+
+    const getSelectedMinimumRating = (rating) => {
+        setMinRating(Number(rating))
+    }
+
     return (
         <div className="homeBox">
-            <h2>Search for routes, crags, areas or users!</h2>
+            <h2>Know what you're looking for? Search for existing routes, crags, areas or users!</h2>
             <input type='text' onChange={(e) => searchAll(e.target.value)}/>
             <div>
                 {allData.map((result, index) => {
                    return <div key={index} onClick={() => goToChosenData(result)}>{result.name? result.name : result.area? result.area : result.crag? result.crag : result.climb? result.climb : "Oops! Can't display result"}</div>
                 })}
             </div>
+            <h2>Explore Ascents database to find your dream climbs</h2>
+            <div class="exploreClimbsBox">
+                <div>
+                    <label htmlFor='type'>Type:</label>
+                    <select name='type' id='type' value={type} onChange={(e) => getSelectedType(e.target.value)}>
+                        <option value=''></option>
+                        <option value='Bouldering'>Bouldering</option>
+                        <option value='Sport Climbing'>Sport Climbing</option>
+                        <option value='Trad Climbing'>Trad Climbing</option>
+                    </select>
+                </div>
+                <div>  
+                    <p>Grade</p>
+                    <label htmlFor="gradefrom">From:</label>
+                    <select name='gradefrom' id='gradefrom' value={gradeFrom} onChange={(e) => getSelectedGradeFrom(e.target.value)}>
+                        <option value=''></option>
+                        <option value='5'>5</option>
+                        <option value='6a'>6a</option>
+                        <option value='6a+'>6a+</option>
+                        <option value='6b'>6b</option>
+                        <option value='6b+'>6b+</option>
+                        <option value='6c'>6c</option>
+                        <option value='6c+'>6c+</option>
+                        <option value='7a'>7a</option>
+                        <option value='7a+'>7a+</option>
+                        <option value='7b'>7b</option>
+                        <option value='7b+'>7b+</option>
+                        <option value='7c'>7c</option>
+                        <option value='7c+'>7c+</option>
+                        <option value='8a'>8a</option>
+                        <option value='8a+'>8a+</option>
+                        <option value='8b'>8b</option>
+                        <option value='8b+'>8b+</option>
+                        <option value='8c'>8c</option>
+                        <option value='8c+'>8c+</option>
+                        <option value='9a'>9a</option>
+                        <option value='9a+'>9a+</option>
+                        <option value='9b'>9b</option>
+                        <option value='9b+'>9b+</option>
+                        <option value='9c'>9c+</option>
+                        <option value='9c+'>9c+</option>
+                    </select>
+                    <label htmlFor="gradeto">To:</label>
+                    <select name='gradeto' id='gradeto' value={gradeTo} onChange={(e) => getSelectedGradeTo(e.target.value)}>
+                        <option value=''></option>
+                        <option value='5'>5</option>
+                        <option value='6a'>6a</option>
+                        <option value='6a+'>6a+</option>
+                        <option value='6b'>6b</option>
+                        <option value='6b+'>6b+</option>
+                        <option value='6c'>6c</option>
+                        <option value='6c+'>6c+</option>
+                        <option value='7a'>7a</option>
+                        <option value='7a+'>7a+</option>
+                        <option value='7b'>7b</option>
+                        <option value='7b+'>7b+</option>
+                        <option value='7c'>7c</option>
+                        <option value='7c+'>7c+</option>
+                        <option value='8a'>8a</option>
+                        <option value='8a+'>8a+</option>
+                        <option value='8b'>8b</option>
+                        <option value='8b+'>8b+</option>
+                        <option value='8c'>8c</option>
+                        <option value='8c+'>8c+</option>
+                        <option value='9a'>9a</option>
+                        <option value='9a+'>9a+</option>
+                        <option value='9b'>9b</option>
+                        <option value='9b+'>9b+</option>
+                        <option value='9c'>9c+</option>
+                        <option value='9c+'>9c+</option>
+                    </select>
+                </div>
+                <div>
+                    <label htmlFor="minrating">Minimum rating:</label>
+                    <select name='minrating' id='minrating' value={minRating} onChange={(e) => getSelectedMinimumRating(e.target.value)}>
+                        <option value='0'></option>
+                        <option value='1'>1 star</option>
+                        <option value='2'>2 stars</option>
+                        <option value='3'>3 stars</option>
+                    </select>
+                </div>
+                <button>Find Climbs</button>
+            </div>
             <h2>Featured Awesome Climbs</h2>
-            <div>
+            <div className="goodClimbsBox">
                 {goodClimbs.map((result, index) => {
-                    return <div key={index} onClick={() => goToGoodClimb(result)}>{result.climb}</div>
+                    return  <div key={index} onClick={() => goToGoodClimb(result)} className="goodClimb">
+                                <div className="goodClimbTop">{result.climb} - {result.grade}</div>
+                                <div className="goodClimbBottom">{result.crag} - {result.area} - {result.country}</div>
+                            </div>
                 })}
             </div>
         </div>
