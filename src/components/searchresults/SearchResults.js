4b81d379f6333a0d1a04c87e78e1cce1e5b2f23b
edouard <edouard.blais@hotmail.com>
2022-10-16 21:23:01 -0400

    continued and styled searchResults component

--- src/components/searchresults/SearchResults.js
+++ src/components/searchresults/SearchResults.js
@@ -1,14 +1,33 @@
 import React from 'react';
 import { useLocation } from "react-router-dom";
+import './SearchResultsBox.css';
 
 const SearchResults = () => {
     const location = useLocation();
     const searchResults= location.state.searchResults;
 
     return (
-        <div>
+        <div className='searchResultsBox'>
             {searchResults.map((climb, index) => {
-                return <div key={index}>{climb.climb}</div>
+                return  <div key={index} className='searchResult'>
+                            <div className='searchResultSubPart'>
+                                <div className='searchResultMainInfo'>{climb.climb}</div>
+                                <div className='searchResultSecInfo'>{climb.crag} - {climb.area} - {climb.country}</div>
+                            </div>
+                            <div className='searchResultSubPart'>
+                                <div className='searchResultMainInfo'>{climb.grade}</div>
+                                <div className='searchResultSecInfo'>{climb.type}</div>
+                            </div>
+                            <div className='searchResultSubPart'>
+                                <div className='searchResultMainInfo'>{climb.averagerating} stars</div>
+                                <div className='searchResultSecInfo'>{climb.numberoflogs} ascents</div>
+                            </div>
+                            <div className='searchResultsButtonContainer'>
+                                <button className='searchResultsButton'>More Info</button>
+                                <button className='searchResultsButton'>Tick it!</button>
+                                <button className='searchResultsButton'>To do!</button>
+                            </div>
+                        </div>
             })}
         </div>
     )
