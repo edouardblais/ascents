00a40f69997eef5c55e2bbcb62888900ee5b7f99
edouard <edouard.blais@hotmail.com>
2022-10-23 15:29:10 -0400

    started refactoring and styling otherUserProfile component

--- src/components/profile/OtherUser/OtherUserProfile.js
+++ src/components/profile/OtherUser/OtherUserProfile.js
@@ -1,9 +1,18 @@
 import React, { useEffect, useState } from 'react';
-import { Link, useLocation } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 import { auth, addToFollowing, addToFollower, getUserInfo, removeFromFollowers, removeFromFollowing } from '../../firebase/Firebase';
 import { useAuthState } from 'react-firebase-hooks/auth';
+import format from 'date-fns/format';
+import formatDistanceToNowStrict from 'date-fns/formatDistanceToNowStrict';
+import { shuffleArray } from '../../operations/Operations';
+import RoutesLogged from '../ProfileInfo/RoutesLogged';
+import BouldersLogged from '../ProfileInfo/BouldersLogged';
+import Following from '../ProfileInfo/Following';
+import ToDo from '../ProfileInfo/ToDo';
+import '../Profile.css';
 
 const OtherUserProfile = () => {
+    const navigate = useNavigate();
     const location = useLocation();
     const otherUser = location.state.otherUserInfo;
 
@@ -11,6 +20,36 @@ const OtherUserProfile = () => {
     const [userInfo, setUserInfo] = useState('');
 
     const [followed, setFollowed] = useState(false);
+
+    const [displayInfo, setDisplayInfo] = useState(true);
+    const [displayRoutesLogged, setDisplayRoutesLogged] = useState(false);
+    const [displayBouldersLogged, setDisplayBouldersLogged] = useState(false);
+    const [displayFollowers, setDisplayFollowers] = useState(false);
+    const [displayToDo, setDisplayToDo]= useState(false);
+
+    const [createdOn, setCreatedOn] = useState('');
+    const [lastSignIn, setLastSignIn] = useState('');
+    const [numberOfAscents, setNumberOfAscents] = useState(0);
+    const [numberOfToDos, setNumberOfToDos] = useState(0);
+    const [recommendedClimbs, setRecommendedClimbs] = useState([]);
+
+    let counter = 0;
+
+    useEffect(() => {
+        setNumberOfAscents(otherUser.logbook.length);
+        setNumberOfToDos(otherUser.todolist.length);
+
+        const recommendedArray = otherUser.logbook.filter((climb) => (climb.recommendation === true));
+        const shuffledRecommendations = shuffleArray(recommendedArray);
+        setRecommendedClimbs(shuffledRecommendations);
+    }, [])
+
+    useEffect(() => {
+        if (user) {
+            setCreatedOn(format(new Date(user.metadata.creationTime), 'PPP'));
+            setLastSignIn(formatDistanceToNowStrict(new Date(user.metadata.lastSignInTime)));
+        }
+    }, [user])
 
     useEffect(() => {
         if (user) {
@@ -52,6 +91,58 @@ const OtherUserProfile = () => {
         alert(`You have unfollowed ${otherUser.name}!`)
     }
 
+    const seeProfileinfo = () => {
+        setDisplayBouldersLogged(false);
+        setDisplayFollowers(false);
+        setDisplayToDo(false);
+        setDisplayRoutesLogged(false);
+        setDisplayInfo(true);
+    }
+
+    const seeRoutesLogged = () => {
+        setDisplayInfo(false);
+        setDisplayBouldersLogged(false);
+        setDisplayFollowers(false);
+        setDisplayToDo(false);
+        setDisplayRoutesLogged(true);
+    }
+
+    const seeBouldersLogged = () => {
+        setDisplayInfo(false);
+        setDisplayFollowers(false);
+        setDisplayToDo(false);
+        setDisplayRoutesLogged(false);
+        setDisplayBouldersLogged(true);
+    }
+
+    const seeFollowing = () => {
+        setDisplayInfo(false);
+        setDisplayBouldersLogged(false);
+        setDisplayRoutesLogged(false);
+        setDisplayToDo(false);
+        setDisplayFollowers(true);
+    }
+
+    const seeToDo = () => {
+        setDisplayInfo(false);
+        setDisplayBouldersLogged(false);
+        setDisplayRoutesLogged(false);
+        setDisplayFollowers(false);
+        setDisplayToDo(true);
+    }
+
+    const goToRecommendedClimb = (climb) => {
+            navigate('/SearchAreas/SearchCrags/SearchClimbs/Climb', {
+                state: {
+                    chosenClimb: climb,
+                }
+            })
+    }
+
+    const increment = () => {
+        counter += 1;
+    }
+
     if (loading) {
         return (
           <div>
@@ -68,35 +159,229 @@ const OtherUserProfile = () => {
         );
     }
 
-    return (
-        <div>
-            <div className='profile box'>
-                <h3>{otherUser.name}</h3>
-                <p>Age: {otherUser.otherinfo.age}</p>
-                <p>Country: {otherUser.otherinfo.country}</p>
-                <p>Started climbing in: {otherUser.otherinfo.startedclimbing}</p>
-                <p>Favorite areas: {otherUser.otherinfo.favoriteareas}</p>
-                <p>Other interests: {otherUser.otherinfo.otherinterests}</p>
-                <p>Following: {otherUser.totalfollowing}</p>
-                <p>Followers: {otherUser.totalfollowers}</p>
-                {followed? <button type='button' onClick={unfollowUser}>Unfollow</button> : <button type='button' onClick={followUser}>Follow</button>}
+    if (displayRoutesLogged) {
+        return (
+            <div className='profileBox'>
+                <div className='profileInfoBox'>
+                    <div className='profileInfoSubBox'>
+                        <h3 className='profileName'>{otherUser.name}</h3>
+                        <p className='profileSubText'>{otherUser.otherInfo.age} years old</p>
+                        <p className='profileSubText'>{otherUser.otherInfo.country}</p>
+                    </div>
+                    <div className='profileInfoSubBox'>
+                        <p className='profileText'>{numberOfAscents} logged ascents</p>
+                        <p className='profileSubText'>{numberOfToDos} dream climbs</p>
+                    </div>
+                    <div className='profileInfoSubBox'>
+                        <p className='profileSubText'>Following {userInfo.totalfollowing} <b>Ascents</b> users</p>
+                        <p className='profileSubText'>Followed by {userInfo.totalfollowers} <b>Ascents</b> users</p>
+                    </div>
+                    <div className='profileInfoSubBox'>
+                        <p className='profileText'><b>Ascents</b> user since</p>
+                        <p className='profileSubText'>{createdOn}</p>  
+                    </div>
+                    <div className='profileInfoSubBox'>
+                        <p className='profileText'>Last seen</p>
+                        <p className='profileSubText'>{lastSignIn} ago</p>
+                    </div>
+                </div>
+                <div className='profileNav'>
+                    <button onClick={seeProfileinfo} className='profileButton'>Profile Info</button>
+                    <div className='activeToggle'>Routes Logged</div>
+                    <button onClick={seeBouldersLogged} className='profileButton'>Boulders Logged</button>
+                    <button onClick={seeToDo} className='profileButton'>To-Do's</button>
+                    <button onClick={seeFollowing} className='profileButton'>Following/Followers</button>
+                </div>
+                <RoutesLogged userInfo={userInfo}/>
             </div>
-            <ul>
-                <Link to='Routes' state={otherUser}>
-                    <li>Routes Logged</li>
-                </Link>
-                <Link to='Boulders' state={otherUser}>
-                    <li>Boulders Logged</li>
-                </Link>
-                <Link to='Following' state={otherUser}>
-                    <li>Following</li>
-                </Link>
-                <Link to='ToDo' state={otherUser}>
-                    <li>To Do List</li>
-                </Link>
-            </ul>
-        </div>
-    )
+        )
+    }
+
+    if (user && displayBouldersLogged) {
+        return (
+            <div className='profileBox'>
+                <div className='profileInfoBox'>
+                    <div className='profileInfoSubBox'>
+                        <h3 className='profileName'>{otherUser.name}</h3>
+                        <p className='profileSubText'>{otherUser.otherinfo.age} years old</p>
+                        <p className='profileSubText'>{otherUser.otherinfo.country}</p>
+                    </div>
+                    <div className='profileInfoSubBox'>
+                        <p className='profileText'>{numberOfAscents} logged ascents</p>
+                        <p className='profileSubText'>{numberOfToDos} dream climbs</p>
+                    </div>
+                    <div className='profileInfoSubBox'>
+                        <p className='profileSubText'>Following {userInfo.totalfollowing} <b>Ascents</b> users</p>
+                        <p className='profileSubText'>Followed by {userInfo.totalfollowers} <b>Ascents</b> users</p>
+                    </div>
+                    <div className='profileInfoSubBox'>
+                        <p className='profileText'><b>Ascents</b> user since</p>
+                        <p className='profileSubText'>{createdOn}</p>  
+                    </div>
+                    <div className='profileInfoSubBox'>
+                        <p className='profileText'>Last seen</p>
+                        <p className='profileSubText'>{lastSignIn} ago</p>
+                    </div>
+                </div>
+                <div className='profileNav'>
+                    <button onClick={seeProfileinfo} className='profileButton'>Profile Info</button>
+                    <button onClick={seeRoutesLogged} className='profileButton'>Routes Logged</button>
+                    <div className='activeToggle'>Boulders Logged</div>
+                    <button onClick={seeToDo} className='profileButton'>To-Do's</button>
+                    <button onClick={seeFollowing} className='profileButton'>Following/Followers</button>
+                </div>
+                <BouldersLogged userInfo={userInfo}/>
+            </div>
+        )
+    }
+
+    if (user && displayFollowers) {
+        return (
+            <div className='profileBox'>
+                <div className='profileInfoBox'>
+                    <div className='profileInfoSubBox'>
+                        <h3 className='profileName'>{otherUser.name}</h3>
+                        <p className='profileSubText'>{otherUser.otherinfo.age} years old</p>
+                        <p className='profileSubText'>{otherUser.otherinfo.country}</p>
+                    </div>
+                    <div className='profileInfoSubBox'>
+                        <p className='profileText'>{numberOfAscents} logged ascents</p>
+                        <p className='profileSubText'>{numberOfToDos} dream climbs</p>
+                    </div>
+                    <div className='profileInfoSubBox'>
+                        <p className='profileSubText'>Following {userInfo.totalfollowing} <b>Ascents</b> users</p>
+                        <p className='profileSubText'>Followed by {userInfo.totalfollowers} <b>Ascents</b> users</p>
+                    </div>
+                    <div className='profileInfoSubBox'>
+                        <p className='profileText'><b>Ascents</b> user since</p>
+                        <p className='profileSubText'>{createdOn}</p>  
+                    </div>
+                    <div className='profileInfoSubBox'>
+                        <p className='profileText'>Last seen</p>
+                        <p className='profileSubText'>{lastSignIn} ago</p>
+                    </div>
+                </div>
+                <div className='profileNav'>
+                    <button onClick={seeProfileinfo} className='profileButton'>Profile Info</button>
+                    <button onClick={seeRoutesLogged} className='profileButton'>Routes Logged</button>
+                    <button onClick={seeBouldersLogged} className='profileButton'>Boulders Logged</button>
+                    <button onClick={seeToDo} className='profileButton'>To-Do's</button>
+                    <div className='activeToggle'>Following/Followers</div>
+                </div>
+                <Following userInfo={userInfo}/>
+            </div>
+        )
+    }
+
+    if (user && displayToDo) {
+        return (
+            <div className='profileBox'>
+                <div className='profileInfoBox'>
+                    <div className='profileInfoSubBox'>
+                        <h3 className='profileName'>{otherUser.name}</h3>
+                        <p className='profileSubText'>{otherUser.otherinfo.age} years old</p>
+                        <p className='profileSubText'>{otherUser.otherinfo.country}</p>
+                    </div>
+                    <div className='profileInfoSubBox'>
+                        <p className='profileText'>{numberOfAscents} logged ascents</p>
+                        <p className='profileSubText'>{numberOfToDos} dream climbs</p>
+                    </div>
+                    <div className='profileInfoSubBox'>
+                        <p className='profileSubText'>Following {userInfo.totalfollowing} <b>Ascents</b> users</p>
+                        <p className='profileSubText'>Followed by {userInfo.totalfollowers} <b>Ascents</b> users</p>
+                    </div>
+                    <div className='profileInfoSubBox'>
+                        <p className='profileText'><b>Ascents</b> user since</p>
+                        <p className='profileSubText'>{createdOn}</p>  
+                    </div>
+                    <div className='profileInfoSubBox'>
+                        <p className='profileText'>Last seen</p>
+                        <p className='profileSubText'>{lastSignIn} ago</p>
+                    </div>
+                </div>
+                <div className='profileNav'>
+                    <button onClick={seeProfileinfo} className='profileButton'>Profile Info</button>
+                    <button onClick={seeRoutesLogged} className='profileButton'>Routes Logged</button>
+                    <button onClick={seeBouldersLogged} className='profileButton'>Boulders Logged</button>
+                    <div className='activeToggle'>To-Do's</div>
+                    <button onClick={seeFollowing} className='profileButton'>Following/Followers</button>
+                </div>
+                <ToDo userInfo={userInfo}/>
+            </div>
+        )
+    }
+
+    if (displayInfo) {
+        return (
+            <div className='profileBox'>
+                <div className='profileInfoBox'>
+                    <div className='profileInfoSubBox'>
+                        <h3 className='profileName'>{otherUser.name}</h3>
+                        <p className='profileSubText'>{otherUser.otherinfo.age} years old</p>
+                        <p className='profileSubText'>{otherUser.otherinfo.country}</p>
+                    </div>
+                    <div className='profileInfoSubBox'>
+                        <p className='profileText'>{numberOfAscents} logged ascents</p>
+                        <p className='profileSubText'>{numberOfToDos} dream climbs</p>
+                    </div>
+                    <div className='profileInfoSubBox'>
+                        <p className='profileSubText'>Following {userInfo.totalfollowing} <b>Ascents</b> users</p>
+                        <p className='profileSubText'>Followed by {userInfo.totalfollowers} <b>Ascents</b> users</p>
+                    </div>
+                    <div className='profileInfoSubBox'>
+                        <p className='profileText'><b>Ascents</b> user since</p>
+                        <p className='profileSubText'>{createdOn}</p>  
+                    </div>
+                    <div className='profileInfoSubBox'>
+                        <p className='profileText'>Last seen</p>
+                        <p className='profileSubText'>{lastSignIn} ago</p>
+                    </div>
+                </div>
+                <div className='profileNav'>
+                    <div className='activeToggle'>Profile Info</div>
+                    <button onClick={seeRoutesLogged} className='profileButton'>Routes Logged</button>
+                    <button onClick={seeBouldersLogged} className='profileButton'>Boulders Logged</button>
+                    <button onClick={seeToDo} className='profileButton'>To-Do's</button>
+                    <button onClick={seeFollowing} className='profileButton'>Following/Followers</button>
+                </div>
+                <div className='profileContent'>
+                    <div className='contentInfoBox'>
+                        <div className='contentInfoSubBox'>
+                            <div className='contentInfoSubSubBox'>
+                                <p>Name: <b>{otherUser.name}</b></p>
+                                <p>From: <b>{otherUser.otherinfo.country}</b></p>
+                                <p>Age: <b>{otherUser.otherinfo.age}</b></p>
+                            </div>
+                            <div className='contentInfoSubSubBox'>
+                                <p>Started climbing in: <b>{otherUser.otherinfo.startedClimbing}</b></p>
+                                <p>Favorite areas: <b>{otherUser.otherinfo.favoriteAreas}</b></p>
+                                <p>Other interests: <b>{otherUser.otherinfo.otherInterests}</b></p>
+                            </div>
+                            <button type='button' className='profileEditButton' onClick={followed?() => unfollowUser(): () => followUser()}><span className="material-symbols-outlined">settings</span></button>
+                        </div>
+                    </div>
+                    <div className='recommendedBox'>
+                        <h2 className='profileName'>Recommended Climbs</h2>
+                        <div className='recommendedClimbsBox'>
+                            {recommendedClimbs.map((climb, index) => {
+                                increment();
+                                if (counter <= 10) { 
+                                    return  <div key={index} onClick={() => goToRecommendedClimb(climb)} className="recommendedClimb">
+                                                <div className="goodClimbTop">
+                                                    <div>{climb.climb} - {climb.grade}</div>
+                                                </div>
+                                                <div className="goodClimbBottom">
+                                                    <div>{climb.crag} - {climb.area} - {climb.country}</div>
+                                                </div>
+                                            </div>
+                                } 
+                            })}
+                        </div>
+                    </div>
+                </div>
+            </div>
+        )
+    }
 }
 
 export default OtherUserProfile;
