ea9ba82532e7f6b57002c03e7f2ab7c70cb82b34
edouard <edouard.blais@hotmail.com>
2022-11-04 14:38:08 -0400

    styled createNew component

--- src/components/addascent/createnew/CreateNew.js
+++ src/components/addascent/createnew/CreateNew.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react';
+import { Link } from "react-router-dom";
 import { addNewClimb, processCountry, processArea, processCrag, processClimb } from '../../firebase/Firebase';
 import { capitalizeFirstLetter, trimSentence } from '../../operations/Operations';
+import './CreateNew.css';
 
 const CreateNew = () => {
     const [country, setCountry] = useState('');
@@ -103,83 +105,104 @@ const CreateNew = () => {
     }, [errorStatus]);
     
     return (
-        <div>
-            <h1>Add New Climb</h1>
-            <form>
-                <label htmlFor='country'>Country</label>
-                <input type='text' id='country' name='country' value={country} onChange={(e) => defineCountry(e.target.value)}/>
-                <div>
-                    {countriesDisplayed?.map((country, index) => {
-                        return <div key={index} onClick={() => defineCountry(country.country)}>{country.country}</div>
-                    })}
-                </div>
-
-                <label htmlFor='area'>Area</label>
-                <input type='text' id='area' name='area' value={area} onChange={(e) => defineArea(e.target.value)}/>
-                <div>
-                    {areasDisplayed?.map((area, index) => {
-                        return <div key={index} onClick={() => defineArea(area.area)}>{area.area}</div>
-                    })}
-                </div>
-
-                <label htmlFor='crag'>Crag</label>
-                <input type='text' id='crag' name='crag' value={crag} onChange={(e) => defineCrag(e.target.value)}/>
-                <div>
-                    {cragsDisplayed?.map((crag, index) => {
-                        return <div key={index} onClick={() => defineCrag(crag.crag)}>{crag.crag}</div>
-                    })}
-                </div>
-
-                <label htmlFor='climb'>Climb</label>
-                <input type='text' id='climb' name='climb' value={climb} onChange={(e) => defineClimb(e.target.value)}/>
-                <div>
-                    {climbsDisplayed?.map((climb, index) => {
-                        return <div key={index} onClick={() => defineClimb(climb.climb)}>{climb.climb}</div>
-                    })}
-                </div>
-                
-                <label htmlFor='grade'>Grade</label>
-                <select name='grade' id='grade' value={grade} onChange={(e) => getSelectedGrade(e.target.value)}>
-                    <option value=''></option>
-                    <option value='5'>5</option>
-                    <option value='6a'>6a</option>
-                    <option value='6a+'>6a+</option>
-                    <option value='6b'>6b</option>
-                    <option value='6b+'>6b+</option>
-                    <option value='6c'>6c</option>
-                    <option value='6c+'>6c+</option>
-                    <option value='7a'>7a</option>
-                    <option value='7a+'>7a+</option>
-                    <option value='7b'>7b</option>
-                    <option value='7b+'>7b+</option>
-                    <option value='7c'>7c</option>
-                    <option value='7c+'>7c+</option>
-                    <option value='8a'>8a</option>
-                    <option value='8a+'>8a+</option>
-                    <option value='8b'>8b</option>
-                    <option value='8b+'>8b+</option>
-                    <option value='8c'>8c</option>
-                    <option value='8c+'>8c+</option>
-                    <option value='9a'>9a</option>
-                    <option value='9a+'>9a+</option>
-                    <option value='9b'>9b</option>
-                    <option value='9b+'>9b+</option>
-                    <option value='9c'>9c+</option>
-                    <option value='9c+'>9c+</option>
-                </select>
-                
-                <label htmlFor='type'>Type of climbing</label>
-                <select name='type' id='type' value={type} onChange={(e) => getSelectedType(e.target.value)}>
-                    <option value=''></option>
-                    <option value='Bouldering'>Bouldering</option>
-                    <option value='Sport Climbing'>Sport Climbing</option>
-                    <option value='Trad Climbing'>Trad Climbing</option>
-                </select>
-
-                <button type='button' onClick={() => submitForm(country, area, crag, climb, grade, type)}>Add New Climb</button>
-
-                <div>{errorMessage}</div>
-            </form>
+        <div className='addAscentBox'>
+            <h2  className='climbTitle'> Create a new climb!</h2>
+            <div className='warningBox'>
+                <h4 className='createNewSubTitle'><span className="material-symbols-outlined">warning</span>Please check first that the climb you are about to create does not already exists <Link to='../AddAscent' className='linkToComponent'>here!</Link></h4>
+                <h4 className='createNewSubTitle'><span className="material-symbols-outlined">warning</span>Existing countries, areas, crags and climbs will be suggested as you create your new climb</h4>
+            </div>
+            <div className='createNewFormBox'>
+                <form className='createNewForm'>
+                    <div className='modalFormBox'>
+                        <div className='modalFormSubBox'>
+                            <label htmlFor='country' className='modalFormTitles'>Country</label>
+                            <input type='text' id='country' name='country' value={country} onChange={(e) => defineCountry(e.target.value)} className='modalFormInput'/>
+                            <div>
+                                {countriesDisplayed?.map((country, index) => {
+                                    return <div key={index} onClick={() => defineCountry(country.country)}>{country.country}</div>
+                                })}
+                            </div>
+                        </div>
+                        <div className='modalFormSubBox'>
+                            <label htmlFor='area' className='modalFormTitles'>Area</label>
+                            <input type='text' id='area' name='area' value={area} onChange={(e) => defineArea(e.target.value)} className='modalFormInput'/>
+                            <div>
+                                {areasDisplayed?.map((area, index) => {
+                                    return <div key={index} onClick={() => defineArea(area.area)}>{area.area}</div>
+                                })}
+                            </div>
+                        </div>
+                    </div>
+                    <div className='modalFormBox'>
+                        <div className='modalFormSubBox'>
+                            <label htmlFor='crag' className='modalFormTitles'>Crag</label>
+                            <input type='text' id='crag' name='crag' value={crag} onChange={(e) => defineCrag(e.target.value)} className='modalFormInput'/>
+                            <div>
+                                {cragsDisplayed?.map((crag, index) => {
+                                    return <div key={index} onClick={() => defineCrag(crag.crag)}>{crag.crag}</div>
+                                })}
+                            </div>
+                        </div>
+                        <div className='modalFormSubBox'>
+                            <label htmlFor='climb' className='modalFormTitles'>Climb</label>
+                            <input type='text' id='climb' name='climb' value={climb} onChange={(e) => defineClimb(e.target.value)} className='modalFormInput'/>
+                            <div>
+                                {climbsDisplayed?.map((climb, index) => {
+                                    return <div key={index} onClick={() => defineClimb(climb.climb)}>{climb.climb}</div>
+                                })}
+                            </div>
+                        </div>
+                    </div>
+                    <div className='modalFormBox'>
+                        <div className='modalFormSubBox'>
+                            <label htmlFor='grade' className='modalFormTitles'>Grade</label>
+                            <select name='grade' id='grade' value={grade} onChange={(e) => getSelectedGrade(e.target.value)} className='modalFormInput'>
+                                <option value=''></option>
+                                <option value='5'>5</option>
+                                <option value='6a'>6a</option>
+                                <option value='6a+'>6a+</option>
+                                <option value='6b'>6b</option>
+                                <option value='6b+'>6b+</option>
+                                <option value='6c'>6c</option>
+                                <option value='6c+'>6c+</option>
+                                <option value='7a'>7a</option>
+                                <option value='7a+'>7a+</option>
+                                <option value='7b'>7b</option>
+                                <option value='7b+'>7b+</option>
+                                <option value='7c'>7c</option>
+                                <option value='7c+'>7c+</option>
+                                <option value='8a'>8a</option>
+                                <option value='8a+'>8a+</option>
+                                <option value='8b'>8b</option>
+                                <option value='8b+'>8b+</option>
+                                <option value='8c'>8c</option>
+                                <option value='8c+'>8c+</option>
+                                <option value='9a'>9a</option>
+                                <option value='9a+'>9a+</option>
+                                <option value='9b'>9b</option>
+                                <option value='9b+'>9b+</option>
+                                <option value='9c'>9c+</option>
+                                <option value='9c+'>9c+</option>
+                            </select>
+                        </div>
+                        <div className='modalFormSubBox'>
+                            <label htmlFor='type' className='modalFormTitles'>Type of climbing</label>
+                            <select name='type' id='type' value={type} onChange={(e) => getSelectedType(e.target.value)} className='modalFormInput'>
+                                <option value=''></option>
+                                <option value='Bouldering'>Bouldering</option>
+                                <option value='Sport Climbing'>Sport Climbing</option>
+                                <option value='Trad Climbing'>Trad Climbing</option>
+                            </select>
+                        </div>
+                    </div>
+                    <div className='modalFormBox'>
+                        <button type='button' onClick={() => submitForm(country, area, crag, climb, grade, type)}>Add New Climb</button>
+                    </div>
+                    <div className='modalFormBox'>
+                        <div>{errorMessage}</div>
+                    </div>
+                </form>
+            </div>
         </div>
     )
 }
