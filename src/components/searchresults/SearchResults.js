46537c6244fbd7fb36e0513261e19d661d584cca
edouard <edouard.blais@hotmail.com>
2022-11-10 20:42:37 -0500

    styled searchresults component

--- src/components/searchresults/SearchResults.js
+++ src/components/searchresults/SearchResults.js
@@ -56,21 +56,22 @@ const SearchResults = () => {
     return (
         <div className='searchResultsBox'>
             {searchResults.map((climb, index) => {
-                return  <div key={index} className='searchResult'>
-                            <div className='searchResultSubPart'>
-                                <div className='searchResultMainInfo'>{climb.climb}</div>
-                                <div className='searchResultSecInfo'>{climb.crag} - {climb.area} - {climb.country}</div>
-                            </div>
-                            <div className='searchResultSubPart'>
-                                <div className='searchResultMainInfo'>{climb.grade}</div>
-                                <div className='searchResultSecInfo'>{climb.type}</div>
-                            </div>
-                            <div className='searchResultSubPart'>
-                                <div className='searchResultMainInfo'>{climb.averagerating} stars</div>
-                                <div className='searchResultSecInfo'>{climb.numberoflogs} ascents</div>
+                return  <div key={index} className='searchResultBox' onClick={() => seeClimb(climb)}>
+                            <div className='searchResultSubBox'>
+                                <div className='searchResultImportantInfoPart'>
+                                    <div className='searchResultMainInfo'>{climb.climb}</div>
+                                    <div className='searchResultSecInfo'>{climb.crag} - {climb.area} - {climb.country}</div>
+                                </div>
+                                <div className='searchResultSubPart'>
+                                    <div className='searchResultMainInfo'>{climb.grade}</div>
+                                    <div className='searchResultSecInfo'>{climb.type}</div>
+                                </div>
+                                <div className='searchResultSubPart'>
+                                    <div className='searchResultMainInfo'>{climb.averagerating} stars</div>
+                                    <div className='searchResultSecInfo'>{climb.numberoflogs} ascents</div>
+                                </div>
                             </div>
                             <div className='searchResultsButtonContainer'>
-                                <button className='searchResultsButton' onClick={() => seeClimb(climb)}>More Info</button>
                                 <button className='searchResultsButton' onClick={() => showAddAscentModal(climb)}>Tick it!</button>
                                 <button className='searchResultsButton' onClick={() => addToToDo(climb)}>To do!</button>
                             </div>
